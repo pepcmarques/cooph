5a8e66cd2411b28ca1844ca1035f4d865267c593
Paulo Marques <paulo.marques@ubc.ca>
2019-11-18 14:49:07 -0800

    PEP-8

--- coop/settings.py
+++ coop/settings.py
@@ -137,7 +137,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
-
